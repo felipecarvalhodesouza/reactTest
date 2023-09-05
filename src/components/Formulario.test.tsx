9ddcc0aafe4b9c5a4d44c3589dde079d535705c8
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2023-09-04 21:54:52 -0300

    Implementando componente baseado no TDD

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import Formulario from "./Formulario";
 
 // Jest
 
@@ -8,7 +9,7 @@ test('Quando o input está vazio, novos participantes não podem ser adicionados
     render(<Formulario />)
     //Encontrar no DOM o input
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participante');
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
 
     //Encontrar o botão
 
