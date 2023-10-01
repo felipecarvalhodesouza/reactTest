4e8df82edb976a38840a8bf05064c90aabe8fbd6
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2023-09-30 22:22:05 -0300

    useRef e o foco no input

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,6 +1,8 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Formulario from "./Formulario";
+import exp from "constants";
+import { RecoilRoot } from "recoil";
 
 // Jest
 
@@ -22,4 +24,30 @@ test('Quando o input está vazio, novos participantes não podem ser adicionados
     //Garantir que o botão esteja desabilitado
 
     expect(botao).toBeDisabled();
+})
+
+test('Adicionar um participante caso exista um nome preenchido', () =>{
+    render(
+    <RecoilRoot>
+        <Formulario />
+    </RecoilRoot>
+    )
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+    const botao = screen.getByRole('button')
+
+    //Inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Felipe Carvalho'
+        }
+    });
+
+    //Clicar no botão de submit
+    fireEvent.click(botao);
+
+    //Garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus();
+
+    //Garantir que o input não tenha um valor
+    expect(input).toHaveValue('');
 })
